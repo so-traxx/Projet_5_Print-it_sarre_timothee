759fa0d5f0cc6b24c8fae9ec19bae24e9a75d210
Timothée SARRE <timothee.sarre@hotmail.fr>
2023-10-29 21:12:03 +0100

    ajout des eventsListener sur les flèches du slider

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const slides = [
+/*const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -16,3 +16,18 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+*/
+
+//ajout des eventsListener sur les flèches du slider
+
+let flecheGauche = document.querySelector(".arrow_left")
+console.log()
+flecheGauche.addEventListener("click", () => {
+	console.log("clique sur la flèche de gauche")
+})
+
+let flecheDroite = document.querySelector(".arrow_right")
+console.log()
+flecheDroite.addEventListener("click", () => {
+	console.log("clique sur la flèche de droite")
+})
