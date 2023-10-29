692394ffd7c3f1d9d793d9683e51c7531a138b40
Timothée SARRE <timothee.sarre@hotmail.fr>
2023-10-29 23:13:23 +0100

    ajout des bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-/*const slides = [
+//code fournit des base contenant un tableau avec les différentes slides
+const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -15,19 +16,33 @@
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
-*/
+];
 
-//ajout des eventsListener sur les flèches du slider
+//variables globales
 
-let flecheGauche = document.querySelector(".arrow_left")
-console.log()
+let currentSlide = 0;
+
+//ajout des eventsListener sur les flèches du slider et modification du slide
+
+const flecheGauche = document.querySelector(".arrow_left")
 flecheGauche.addEventListener("click", () => {
-	console.log("clique sur la flèche de gauche")
+	console.log("clic sur la flèche de gauche")
+});
+
+const flecheDroite = document.querySelector(".arrow_right")
+flecheDroite.addEventListener("click", () => {
+	console.log("clic sur la flèche de droite")
 })
 
-let flecheDroite = document.querySelector(".arrow_right")
-console.log()
-flecheDroite.addEventListener("click", () => {
-	console.log("clique sur la flèche de droite")
-})
+// ajout des bullet points au slider
+
+const divDots = document.querySelector(".dots")
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("span")
+	dot.classList.add("dot")
+	if (i === currentSlide) {
+		dot.classList.add("dot_selected")
+	};
+	divDots.appendChild(dot)
+}
+
