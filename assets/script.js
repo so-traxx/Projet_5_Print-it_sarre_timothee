285bbf7f6d4e7b8fb19ea2e3a653b1cd06a47613
Timothée SARRE <timothee.sarre@hotmail.fr>
2023-11-05 00:37:47 +0100

    projet fini

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 //code fournit des base contenant un tableau avec les différentes slides
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -21,28 +22,72 @@ const slides = [
 //variables globales
 
 let currentSlide = 0;
+const flecheDroite = document.querySelector(".arrow_right");
+const flecheGauche = document.querySelector(".arrow_left");
+const txtSlide = document.querySelector("#banner p");
+const imgSlide = document.querySelector(".banner-img");
+const divDots = document.querySelector(".dots");
 
-//ajout des eventsListener sur les flèches du slider et modification du slide
+//appel fonctions 
 
-const flecheGauche = document.querySelector(".arrow_left")
+createDots();
+changeDot();
+
+/*
+* ajout des eventsListener sur les flèches du slider
+* ajout de la structure conditionnelle pour défilement infini
+* appel de la fonction de modification du slide au clic sur une flèche
+* affichage dans la console de la flèche utilisée (g ou d)
+*/
+
 flecheGauche.addEventListener("click", () => {
-	console.log("clic sur la flèche de gauche")
+	if (currentSlide === 0) {
+		currentSlide = slides.length -1;
+	} else {
+		currentSlide--;
+	};
+	changeSlide();
+	console.log("clic sur la flèche de gauche");
 });
 
-const flecheDroite = document.querySelector(".arrow_right")
 flecheDroite.addEventListener("click", () => {
-	console.log("clic sur la flèche de droite")
-})
-
-// ajout des bullet points au slider
-
-const divDots = document.querySelector(".dots")
-for (let i = 0; i < slides.length; i++) {
-	let dot = document.createElement("span")
-	dot.classList.add("dot")
-	if (i === currentSlide) {
-		dot.classList.add("dot_selected")
+	if (currentSlide === slides.length -1) {
+		currentSlide = 0;
+	} else {
+		currentSlide++;
 	};
-	divDots.appendChild(dot)
-}
+	changeSlide();
+	console.log("clic sur la flèche de droite");
+});
 
+// ajout des bullet points au slider via une fonction
+
+function createDots () {
+	for (let i = 0; i < slides.length; i++) {
+		let dot = document.createElement("span");
+		dot.classList.add("dot");
+		divDots.appendChild(dot);
+	}
+};
+
+// fonction pour modifier le bullet point en fonction du slide actuel 
+
+function changeDot() {
+	let currentDot = document.querySelectorAll(".dot");
+		for (let i = 0; i < currentDot.length; i++) {
+			let updateDot = currentDot[i];
+			if (i === currentSlide) {
+				updateDot.classList.add('dot_selected');		
+	  		} else {
+				updateDot.classList.remove('dot_selected');	 
+	  		}
+		} 
+};
+
+// modifications du texte, de l'image et du bullet point en fonction du slide actuel
+
+function changeSlide () {
+	imgSlide.setAttribute("src" , "./assets/images/slideshow/"+ slides[currentSlide].image);
+	txtSlide.innerHTML = slides[currentSlide].tagLine;
+	changeDot();
+};
